187308949ae99c34a7ea2fa50cf9f63b5b550c56
Miki Tebeka <miki@353solutions.com>
2024-11-10 16:24:39 +0000

    end of 02_02

--- Ch02/02_02/runes.go
+++ Ch02/02_02/runes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
@@ -10,4 +11,14 @@ func main() {
 	fmt.Println("len:", len(text)) // 8 (not 6)
 	fmt.Printf("text[0]: %c (%T)\n", text[0], text[0])
 	fmt.Printf("text[2]: %c\n", text[2])
+
+	for i, c := range text {
+		fmt.Printf("%d: %c (%T)\n", i, c, c)
+	}
+
+	fmt.Println("len (runes):", utf8.RuneCountInString(text))
+
+	rs := []rune(text)
+	fmt.Println("len ([]rune):", len(rs))
+	fmt.Printf("rs[2]: %c\n", rs[2])
 }
