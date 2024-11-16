5009f218252b1999c75f2882afb38a1b249652f3
Miki Tebeka <miki@353solutions.com>
2024-11-16 15:23:00 +0000

    04_02

--- Ch04/04_02/convert.go
+++ Ch04/04_02/convert.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+
+	"golang.org/x/net/html/charset"
 )
 
 func main() {
@@ -15,4 +17,25 @@ func main() {
 	defer in.Close()
 
 	io.CopyN(os.Stdout, in, 100)
+
+	fmt.Println("------")
+	in.Seek(0, io.SeekStart)
+	enc16, _ := charset.Lookup("UTF-16")
+	r := enc16.NewDecoder().Reader(in)
+	io.CopyN(os.Stdout, r, 100)
+
+	in.Seek(0, io.SeekStart)
+	out, err := os.Create("holmes-latin1.text")
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	defer out.Close()
+
+	latin1, _ := charset.Lookup("LATIN1")
+	w := latin1.NewEncoder().Writer(out)
+	if _, err := io.Copy(w, r); err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
 }
