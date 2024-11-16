217745ea3e895e2d0508994eb1c881fb4683ca0d
Miki Tebeka <miki@353solutions.com>
2024-11-16 15:46:33 +0000

    04_04

--- Ch04/04_04/search.go
+++ Ch04/04_04/search.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"golang.org/x/text/unicode/norm"
 )
 
 type Tour struct {
@@ -14,6 +16,7 @@ type Tour struct {
 var db []Tour
 
 func insert(city string, year, month, day int) {
+	city = norm.NFKC.String(city)
 	tour := Tour{
 		City: city,
 		Date: time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC),
@@ -22,6 +25,7 @@ func insert(city string, year, month, day int) {
 }
 
 func searchByCity(city string) []Tour {
+	city = norm.NFKC.String(city)
 	var matches []Tour
 	for _, t := range db {
 		if t.City == city {
@@ -35,6 +39,9 @@ func searchByCity(city string) []Tour {
 func main() {
 	city1 := "Kraków"
 	city2 := "Kraków"
+
+	fmt.Println(len(city1), len(city2))
+	fmt.Println(city1 == norm.NFKC.String(city2))
 
 	insert(city1, 2025, 5, 17)
 	fmt.Println(searchByCity(city2))
